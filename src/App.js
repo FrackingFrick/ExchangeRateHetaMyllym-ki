10e59027832d157410a958fac8c57ec679990623
FrackingFrick <heta.myllymaki@gmail.com>
2022-09-22 15:43:12 +0300

    App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useState} from 'react'
+const URL='http://api.exchangeratesapi.io/v1/latest?access_key='
+const API_KEY='911b3806a1cbe40dacf96b52c007b3f3'
 function App() {
+  const [yen,setYen]=useState(0)
+  const [eur,setEur]=useState(0)
+  const [rate,setRate]=useState(0)
+
+  async function convert(e){
+    e.preventDefault();
+    try {
+      const address=URL+API_KEY
+      const response=await fetch(address)
+
+      if(response.ok){
+        const json=await response.json()
+        console.log(json.rates.JPY)
+        setRate(json.rates.JPY)
+
+        setEur(yen/json.rates.JPY)
+      }else {
+        alert ('Error retrieving exchange rates')
+        console.log(response)
+      }
+    }catch (err){
+      alert(err)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div id='container'>
+    <h1>JPY to EUR calculator</h1>
+    <form onSubmit={convert}>
+      <div>
+        <label>JPY </label>&nbsp;
+        <input type='number' step='0.01'
+        value={yen} onChange={e=>setYen(e.target.value)} />
+        <output>{rate}</output>
+      </div>
+      <div>
+        <label>EUR </label>
+        <output>{eur.toFixed(2)} €</output>
+      </div>
+      <div>
+        <button>Calculate</button>
+      </div>
+    </form>
+  </div>
   );
 }
 
